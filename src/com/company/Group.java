60b383eed01e446f9156534effcbc32bfeb34a27
VSurguchev <surguchev@gmail.com>
2021-03-24 22:37:31 +0200

    done full homeWork

--- src/com/company/Group.java
+++ src/com/company/Group.java
@@ -1,4 +1,106 @@
 package com.company;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class Group {
+    
+ ListGroup listGroup = new ListGroup();
+
+    private Student student;
+    private String nameCourse;
+    private String dateStart;
+    private int sumLessons;
+    private int countLessonsPerMonth;
+    private String surname;
+    private ArrayList<Student> personList = new ArrayList<Student>();
+    private Object Group;
+
+    public Group(String nameCourse, String dateStart, int sumLessons, int countLessonsPerMonth, ArrayList<Student> personList) {
+        this.nameCourse = nameCourse;
+        this.dateStart = dateStart;
+        this.sumLessons = sumLessons;
+        this.countLessonsPerMonth = countLessonsPerMonth;
+        this.personList = personList;
+    }
+
+    public Group(Student student) {
+        this.student = student;
+    }
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+    
+    public int getSumLessons() {
+        return sumLessons;
+    }
+
+    public int getCountLessonsPerMonth() {
+        return countLessonsPerMonth;
+    }
+
+    public ArrayList<Student> getPersonList() {
+        return personList;
+    }
+
+    public String getFullNameCroup() {
+        return nameCourse + "_" + dateStart;
+    }
+
+    public String getDateLastLesson() {
+        int stepDayForMonth = (int) 7 / countLessonsPerMonth;
+        int lastLesson = stepDayForMonth * sumLessons;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(sdf.parse(dateStart));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        calendar.add(Calendar.DATE, lastLesson);
+        return sdf.format(calendar.getTime());
+    }
+
+    public String getListStudent() {
+        String listStudent = "";
+        for (int i = 0; i < personList.size(); i++) {
+            listStudent += (personList.get(i)).getName().charAt(0) + "." + (personList.get(i)).getSurname() + "\n";
+        }
+        return listStudent;
+    }
+
+    public void addStudent(Student student) {
+        personList.add(student);
+        listGroup.addGroup(Group);
+    }
+    
+        
+
+    public void addTeacher(Student teacher){
+        personList.add(teacher);
+    }
+
+    public String checkStudent(String surname) {
+        for (int i = 0; i < personList.size(); i++) {
+            if (surname.equals((personList.get(i)).getSurname())) {
+                return (personList.get(i)).getTypePerson() + ": "
+                        + (personList.get(i)).getSurname() + ' '
+                        + (personList.get(i)).getName() + '\n'
+                        + (personList.get(i)).getDateBirthday() + '\n'
+                        + (personList.get(i)).getNumberPhone();
+            }
+        }
+        return "Student not found";
+    }
+
+    public void deleteStudent(String numberOrSurname) {
+        for (int i = 0; i < personList.size(); i++) {
+            if ((personList.get(i)).getSurname().equals(numberOrSurname) | (personList.get(i)).getNumberPhone().equals(numberOrSurname)) {
+                personList.remove(i);
+            }
+        }
+    }
 }
---
+++ src/com/company/Student.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public class Person {
+    private String surname;
+    private String name;
+    private String dateBirthday;
+    private String numberPhone;
+    private TypePerson typePerson;
+
+    public Person(String surname, String name, String dateBirthday, String number, TypePerson typePerson) {
+        this.surname = surname;
+        this.name = name;
+        this.dateBirthday = dateBirthday;
+        this.numberPhone = number;
+        this.typePerson = typePerson;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateBirthday() {
+        return dateBirthday;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public TypePerson getTypePerson() {
+        return typePerson;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateBirthday(String dateBirthday) {
+        this.dateBirthday = dateBirthday;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public void setTypePerson(TypePerson typePerson) {
+        this.typePerson = typePerson;
+    }
+
+}
